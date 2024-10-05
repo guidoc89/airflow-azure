dc881b2fd4961d1e3d73d05a9099e8b70e542bf1
guido <guid.castr@gmail.com>
2024-10-05 16:54:09 +0200

    refactor(notebooks): separate in helper files

---
+++ notebooks/file_names.py
@@ -0,0 +1,5 @@
+
+class CsvFileName:
+    DF_PLAYERS = "df_players.csv"
+    DF_MATCHES = "df_matches.csv"
+    DF_GS_STATISTICS = "df_gs_statistics.csv"
--- notebooks/silver to gold.py
+++ notebooks/silver to gold.py
@@ -1,80 +1,35 @@
 # Databricks notebook source
 # Imports
-from pyspark.sql.functions import input_file_name, split, col, coalesce, concat_ws, size, when, round, to_date, avg, sum, countDistinct, expr
+from notebooks.file_names import CsvFileName
+from pyspark.sql.functions import (
+    input_file_name,
+    split,
+    col,
+    coalesce,
+    concat_ws,
+    size,
+    when,
+    round,
+    to_date,
+    avg,
+    sum,
+    countDistinct,
+    expr,
+)
 from pyspark.sql.types import IntegerType
-
-# COMMAND ----------
-
-# Schema namespaces
-class PlayersSchema:
-    PLAYER_ID = "player_id"
-    NAME_FIRST = "name_first"
-    NAME_LAST = "name_last"
-    IOC = "ioc"
-    DOB = "dob"
-    
-class PlayersCreatedSchema:
-    FULL_NAME = "full_name"
-    COUNTRY = "country"
-
-class MatchesSchema:
-    WINNER_NAME = "winner_name"
-    LOSER_NAME = "loser_name"
-    WINNER_ID = "winner_id"
-    LOSER_ID = "loser_id"
-    TOURNEY_NAME = "tourney_name"
-    TOURNEY_ID = "tourney_id"
-    SURFACE = "surface"
-    ROUND = "round"
-    TOURNEY_LEVEL = "tourney_level"
-    W_BP_SAVED = "w_bpSaved"
-    W_BP_FACED = "w_bpFaced"
-    L_BP_SAVED = "l_bpSaved"
-    L_BP_FACED = "l_bpFaced"
-    
-class MatchesCreatedSchema:
-    YEAR = "year"
-    PATH = "path"
-    LOSER_FULL_NAME = "loser_full_name"
-    WINNER_FULL_NAME  = "winner_full_name"
-
-
-class PlayersStatisticsSchema:
-    MATCHES_PLAYED = "matches_played"
-    MATCHES_LOST = "matches_lost"
-    MATCHES_WON = "matches_won"
-    WIN_PERCENTAGE = "win_%"
-    LOSS_PERCENTAGE = "loss_%"
-    GS_PLAYED = "gs_played"
-    GS_WON = "gs_won"
-    GS_MATCHES_PLAYED = "gs_matches_played"
-    GS_MATCHES_WON = "gs_matches_won"
-    GS_MATCHES_LOST = "gs_matches_lost"
-    GS_MATCHES_WIN_PERCENTAGE = "gs_matches_win_%"
-    MASTERS_1000_MATCHES_PLAYED = "masters_1000_matches_played"
-    FINALS_PLAYED = "finals_played"
-    MASTERS_1000_PLAYED = "masters_1000_played"
-    DAVIS_CUP_PLAYED = "davis_cup_played"
-    CARRER_W_BP_SAVED_PERCENTAGE = "carrer_wins_bp_saved_%"  # in their won matches, how many of them did they save?
-    CARRER_L_BP_SAVED_PERCENTAGE = "carrer_losses_bp_saved_%"  # in their lost matches, how many of them did they save?
-    CARRER_AVG_BP_SAVED_PERCENTAGE = "carrer_avg_bp_saved_%"  # avg of the above two
-    # BP_FACED_PERCENTAGE = "bp faced %"  # NOTE: cant calculate it, since we cant calculate the total number of points played it seems
-    FINALS_PLAYED_PERCENTAGE = "finals_played_%"
-    FINALS_WON = "finals_won"
-    FINALS_WIN_PERCENTAGE = "finals_win_%"
-    GS_WIN_PERCENTAGE = "gs_win_%"
-    MASTERS_1000_WIN_PERCENTAGE = "masters_1000_win_%"
-    DAVIS_WIN_PERCENTAGE = "davis_win_%"
-    TOURNAMETS_PLAYED = "tournaments_played"
-    TOURNAMENT_WIN_PERCENTAGE = "tournament_win_%"
-
+from paths import SILVER_PATH, GOLD_PATH
+from schemas import (
+    PlayersSchema,
+    PlayersCreatedSchema,
+    PlayersStatisticsSchema,
+    MatchesSchema,
+    MatchesCreatedSchema,
+)
+from cols_uniques import TourneyLevel, Round
+# fmt: off
 
 
 # COMMAND ----------
-
-
-SILVER_PATH = "/mnt/silver"
-GOLD_PATH = "/mnt/gold"
 
 silver_files = dbutils.fs.ls(SILVER_PATH)
 matches_csv_path = [file.path for file in silver_files if "matches" in file.name][0]
@@ -188,14 +143,6 @@ df_players_statistics = df_players_statistics.join(
 # FINALS_APPEARANCES_PERCENTAGE = "finals appearances %"
 # FINALS_WIN_PERCENTAGE = "finals win %"
 # GS_WIN_PERCENTAGE = "gs win %"
-class TourneyLevel:
-    GS = "G"
-    MASTER_1000 = "M"    
-    DAVIS = "D"
-    TOUR_FINALS = "F"
-class Round:
-    F = "F"
-    SF = "SF"
 
 # COMMAND ----------
 
@@ -390,6 +337,6 @@ df_gs_statistics = df_gs_statistics.withColumn(
 # COMMAND ----------
 
 # Save them as csv (TODO: use delta format later) in the silver layer
-df_players_statistics.write.mode("overwrite").option("header", "true").format("csv").save(f"{GOLD_PATH}/df_players.csv")
-df_gs_statistics.write.mode("overwrite").option("header", "true").format("csv").save(f"{GOLD_PATH}/df_gs_statistics.csv")
-df_matches.write.mode("overwrite").option("header", "true").format("csv").save(f"{GOLD_PATH}/df_matches.csv")
+df_players_statistics.write.mode("overwrite").option("header", "true").format("csv").save(f"{GOLD_PATH}/{CsvFileName.DF_PLAYERS}")
+df_gs_statistics.write.mode("overwrite").option("header", "true").format("csv").save(f"{GOLD_PATH}/{CsvFileName.DF_GS_STATISTICS}")
+df_matches.write.mode("overwrite").option("header", "true").format("csv").save(f"{GOLD_PATH}/{CsvFileName.DF_MATCHES}")

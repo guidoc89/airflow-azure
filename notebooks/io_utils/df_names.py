60ff8abd0179e9cdd1da22579cb3c4d9efaad655
guido <guid.castr@gmail.com>
2024-10-06 19:03:16 +0200

    refactor(io): add factory pattern for csv/delta lake storage IO handling

---
+++ notebooks/io_utils/df_names.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class DataFrameNames(Enum):
+    ATP_PLAYERS = "atp_players"  # name of the csv downloaded from GitHub
+    DF_PLAYERS = "df_players"
+    DF_MATCHES = "df_matches"
+    DF_GS_STATISTICS = "df_gs_statistics"
+    DF_MASTERS_STATISTICS = "df_masters_statistics"
+
---
+++ notebooks/io_utils/paths.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Paths(Enum):
+    BRONZE_PATH = "/mnt/bronze"
+    SILVER_PATH = "/mnt/silver"
+    GOLD_PATH = "/mnt/gold"
